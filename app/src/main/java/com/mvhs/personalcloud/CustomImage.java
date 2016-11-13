a7aae2584bb535aff4f5abf125f5d16d7d8e24dd
Andrew Yates <andrewmacu@me.com>
2016-11-12 16:06:08 -0800

    Fix

--- app/src/main/java/com/mvhs/personalcloud/CustomImage.java
+++ app/src/main/java/com/mvhs/personalcloud/CustomImage.java
@@ -6,30 +6,23 @@ import java.util.Date;
  * Created by tyler on 11/12/2016.
  */
 
-public class CustomImage {
+public class CustomImage
+{
 
-    static int numImages;
-    int id;
-    String name;
-    String localPath;
-    Date date;
+    private int id;
+    private String localPath;
+    private Date date;
 
-    CustomImage(int _id, String _name, String _localPath, Date _date)
+    public CustomImage(int _id, String _localPath, Date _date)
     {
         id = _id;
-        name = _name;
         localPath = _localPath;
         date = _date;
-        numImages++;
     }
 
     public int getImageId()
     {
         return(id);
-    }
-    public String getImageName()
-    {
-        return(name);
     }
     public String getImagePath()
     {
